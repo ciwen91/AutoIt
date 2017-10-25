17c0f6ff3da5b08261c6ff3e46dfee356205d83f
ciwen91 <409738465@qq.com>
2017-10-25 08:05:54 +0800

    1

---
+++ AutoIt.Foundation/AutoIt.Foundation.Store/DBStore/EFStore.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using StoreCenter;
+
+namespace AutoIt.Foundation.StoreCenter.DBStore
+{
+    public class EFStore<T> : IQueryableDataMedia<T> where T : EntityBase
+    {
+        protected DbContext _DbContext=new DbContext("");
+
+        #region IDataMedia<T>
+
+        public IEnumerable<T> Get()
+        {
+            var group = _DbContext.Set<T>().ToList();
+            return group;
+        }
+
+        public IEnumerable<T> Get(IEnumerable<string> keyGroup)
+        {
+            var group = _DbContext.Set<T>()
+                .Where(item => keyGroup.Contains(item.Key_))
+                .ToList();
+            return group;
+        }
+
+        public void Add(IEnumerable<T> group)
+        {
+            _DbContext.Set<T>().AddRange(group);
+            _DbContext.SaveChanges();
+        }
+
+        public void Update(IEnumerable<T> @group)
+        {
+            foreach (var item in group)
+            {
+                _DbContext.Entry(item).State = EntityState.Modified;
+            }
+            
+            _DbContext.SaveChanges();
+        }
+
+        public void Delete(IEnumerable<string> keyGroup)
+        {
+           //_DbContext.Set<T>()
+           //     .Where(item => keyGroup.Contains(item.Key_))
+               
+        }
+
+        public IEnumerable<string> Exist(IEnumerable<string> keyGroup)
+        {
+            var group = _DbContext.Set<T>()
+                .Where(item => keyGroup.Contains(item.Key_))
+                .Select(item => item.Key_)
+                .ToList();
+
+            return group;
+        }
+
+        public int Count()
+        {
+            var count = _DbContext.Set<T>().Count();
+            return count;
+        }
+
+        #endregion
+
+        #region IQueryable<T>
+
+        public IEnumerable<T> Get(IQueryable<T> query)
+        {
+            return query.ToList();
+        }
+
+        public void Update(IQueryable<T> query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(IQueryable<T> query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count(IQueryable<T> query)
+        {
+            return query.Count();
+        }
+
+        #endregion
+
+
+    }
+}
