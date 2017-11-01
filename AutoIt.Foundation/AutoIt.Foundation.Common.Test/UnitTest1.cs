008b3cabb34f7d4245ed6b30dadb913e14c29880
Administrator <Administrator@AFOGL-703071853>
2017-11-01 13:40:55 +0800

    1

--- AutoIt.Foundation/AutoIt.Foundation.Common.Test/UnitTest1.cs
+++ AutoIt.Foundation/AutoIt.Foundation.Common.Test/UnitTest1.cs
@@ -1,17 +1,28 @@
 ﻿using System;
 using AutoIt.Foundation.Common.Context;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace AutoIt.Foundation.Common.Test
 {
     public class DepencyDataTest
     {
+        private ITestOutputHelper _Output;
+
+        public DepencyDataTest(ITestOutputHelper output)
+        {
+            _Output = output;
+        }
+
         [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
         public void Test(int a)
         {
             var depency = new DependcyData<int>();
 
-            Console.WriteLine(depency.GetData());
+            Console.WriteLine("01");
+            _Output.WriteLine(depency.GetData().ToString());
         }
     }
 }
