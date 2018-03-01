c6e8669108db07f17d43106aacbd86c02af02710
ciwen91 <ciwen91@WIN-2R9MK3G47RA>
2018-03-01 23:05:06 +0800

    1

---
+++ AutoIt.Foundation/AutoIt.Foundation.TestBase/ITestOutput.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoIt.Foundation.TestBase
+{
+    /// <summary>
+    /// 输出接口
+    /// </summary>
+    public interface ITestOutput
+    {
+        /// <summary>
+        /// 输出信息
+        /// </summary>
+        /// <param name="obj">输出内容</param>
+        /// <param name="caseName">用例名称</param>
+        void WriteLine(object obj, string caseName = null);
+    }
+}
---
+++ AutoIt.Foundation/AutoIt.Foundation.TestBase/UnitTestBase.cs
@@ -0,0 +1,44 @@
+﻿using Xunit.Abstractions;
+
+namespace AutoIt.Foundation.TestBase
+{
+    /// <summary>
+    /// xUnit单元测试基类
+    /// </summary>
+    public abstract class UnitTestBase : ITestOutput
+    {
+        /// <summary>
+        /// xUnit输出接口
+        /// </summary>
+        private readonly ITestOutputHelper _Output;
+
+        /// <summary>
+        /// 用例名称
+        /// </summary>
+        public string CaseName => this.GetType().Name
+                                  + (string.IsNullOrEmpty(SubCaseName)  ? string.Empty : "." + SubCaseName);
+        /// <summary>
+        /// 子用例名称
+        /// </summary>
+        public string SubCaseName { get; set; }
+
+        protected UnitTestBase(ITestOutputHelper output)
+        {
+            this._Output = output;
+        }
+
+        #region ITestOutput
+
+        /// <summary>
+        /// 输出函数
+        /// </summary>
+        /// <param name="str">输出内容</param>
+        /// <param name="caseName">用例名称</param>
+        public void WriteLine(object obj, string caseName = null)
+        {
+            _Output?.WriteLine(obj.ToString());
+        }
+
+        #endregion
+    }
+}
